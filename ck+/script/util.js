a9c771bb1ab1b2d5b791932fbce7cb7a0f07f44f
emilyploszaj <emilyploszaj@gmail.com>
2026-08-22 18:44:04 -0500

    Fix image lookup for ck+

--- ck+/script/util.js
+++ ck+/script/util.js
@@ -81,8 +81,8 @@ function getPokeImage(poke, size, unownExtra = undefined) {
 	if (game.name == "pk") {
 		return 'https://img.pokemondb.net/sprites/platinum/' + shiny + '/' + poke + '.png';
 	}
-	return `./images/pokemon/crystal/${poke}.png`;
-	// return 'https://img.pokemondb.net/sprites/crystal/' + shiny + '/' + poke + '.png';
+	// return `./images/pokemon/crystal/${poke}.png`;
+	return 'https://img.pokemondb.net/sprites/crystal/' + shiny + '/' + poke + '.png';
 }
 
 function getTargeting(move) {
